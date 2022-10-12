6391fe1cb400810b24b226b373fbb08743083d69
Timadey <dlktimothy@gmail.com>
2022-10-12 20:21:04 +0100

    Edit SQL files

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,9 @@
 -- Create new database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Create new user hbnb_dev
-CREATE IF NOT EXISTS USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant hbnb_dev all priveldge on hbnb_dv_db
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+USE hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
