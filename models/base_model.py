e62ad3bba0fb3a34fed920c880671e0b73a89755
Timadey <dlktimothy@gmail.com>
2022-10-13 00:40:47 +0100

    Fix Base Model init and Column Errors

--- models/base_model.py
+++ models/base_model.py
@@ -18,25 +18,19 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            if 'updated_at' in kwargs:
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                kwargs['updated_at'] = datetime.now()
-            if 'created_at' in kwargs:
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
-            else:
-                kwargs['created_at'] = datetime.now()
-            if '__class__' in kwargs:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        if kwargs:
+            if '__class___' in kwargs:
                 del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for attr, value in kwargs.items():
+                if attr == 'updated_at' or attr == 'created_at':
+                    self.__setattr__(attr, datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    self.__setattr__(attr, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
