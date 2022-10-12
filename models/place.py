5ba03d768819a9cf995e7099bbc60d3e192921de
Timadey <dlktimothy@gmail.com>
2022-10-12 20:40:08 +0100

    Fix Column Name Arg in place_amenity Table

--- models/place.py
+++ models/place.py
@@ -9,20 +9,24 @@ from sqlalchemy.orm import relationship
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Integer, nullable=True)
+    city_id = Column('city_id', String(
+        60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column('user_id', String(
+        60), ForeignKey('users.id'), nullable=False)
+    name = Column('name', String(128), nullable=False)
+    description = Column('description', String(1024), nullable=True)
+    number_rooms = Column('number_rooms', Integer, nullable=False, default=0)
+    number_bathrooms = Column(
+        'number_bathrooms', Integer, nullable=False, default=0)
+    max_guest = Column('max_guest', Integer, nullable=False, default=0)
+    price_by_night = Column('price_by_night', Integer,
+                            nullable=False, default=0)
+    latitude = Column('latitude', Float, nullable=True)
+    longitude = Column('longitude', Integer, nullable=True)
     reviews = relationship('Review', uselist=True,
                            backref='place', cascade='all')
-    place_amenity = Table('place_amenity', Base.metadata, Column(String(60), ForeignKey('places.id'), primary_key=True,
-                          nullable=False), Column(String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
+    place_amenity = Table('place_amenity', Base.metadata, Column('place_id', String(60), ForeignKey('places.id'), primary_key=True,
+                          nullable=False), Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
     amenities = relationship(
         'Amenity', secondary="place_amenity", viewonly=False)
     amenity_ids = []
