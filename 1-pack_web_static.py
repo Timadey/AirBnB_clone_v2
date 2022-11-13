1239694de47493600bdc49c57b103d2defd6ca5a
Timadey <dlktimothy@gmail.com>
2022-11-13 03:36:22 +0100

    luis do pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""This module uses Fabric to archive the repo contents
-of AirBNB Clone Repo which will be deployed later on
-"""
-
-import fabric
+""" Function that compress a folder """
 from datetime import datetime
+from fabric.api import local
+import os
 
 
 def do_pack():
     try:
-        # Create the directory to save the archive
-        fabric.operations.local("mkdir -p versions")
-        n = datetime.now()
-        archive = f"versions/web_static_{n.strftime('%Y%m%d%H%M%S')}"
-        # Get the archive
-        fabric.operations.local(f"tar -cvzf {archive} web_static ")
-        return archive
-    except Exception:
+        if not os.path.exists("versions"):
+            local('mkdir versions')
+        t = datetime.now()
+        f = "%Y%m%d%H%M%S"
+        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        return archive_path
+    except:
         return None
